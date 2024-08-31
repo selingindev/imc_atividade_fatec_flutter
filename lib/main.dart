64ab0ef0f094064629877083b5041f0b3ddc9eb9
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-31 14:38:41 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:imc_atividade_fatec_flutter/src/UI/pages/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ lib/src/UI/pages/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:imc_atividade_fatec_flutter/src/UI/pages/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo home Page'),
+    );
+  }
+}

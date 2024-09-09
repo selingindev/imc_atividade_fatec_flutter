1d3a9db6c4a4c72264ef6518ae92e43d1d78fcaf
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-09 17:52:33 -0300

    instanciando

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:imc_atividade_fatec_flutter/core/calculate_imc_usecase.dart';
 import 'package:imc_atividade_fatec_flutter/src/UI/pages/app_widget.dart';
+import 'package:imc_atividade_fatec_flutter/src/aplication/controllers/imc_controller.dart';
+import 'package:imc_atividade_fatec_flutter/src/data/repositories/imc_repository.dart';
 
 void main() {
-  runApp(const AppWidget());
+   // Criação da instância do repositório
+  final imcRepository = ImcRepository();
+  
+  // Criação da instância do caso de uso
+  final calculateIMCUseCase = CalculateImcUsecase(imcRepository);
+  
+  // Criação da instância do controlador
+  final imcController = ImcController(calculateIMCUseCase);
+
+  runApp(AppWidget(controller: imcController));
 }
 
 

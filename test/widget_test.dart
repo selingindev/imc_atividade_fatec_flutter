cb4523d43455f39b35b6d4382871e628c9a5f5d6
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-09 21:15:18 -0300

    arrumando test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,25 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:imc_atividade_fatec_flutter/core/calculate_imc_usecase.dart';
 import 'package:imc_atividade_fatec_flutter/src/UI/pages/app_widget.dart';
+import 'package:imc_atividade_fatec_flutter/src/aplication/controllers/imc_controller.dart';
+import 'package:imc_atividade_fatec_flutter/src/data/repositories/imc_repository.dart';
 
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const AppWidget());
+ 
+    final imcRepository = ImcRepository();
+  
+  // Criação da instância do caso de uso
+  final calculateIMCUseCase = CalculateImcUsecase(imcRepository);
+  
+  // Criação da instância do controlador
+  final imcController = ImcController(calculateIMCUseCase);
+    // Build our app and trigger a frame.    final ImcController controller;
+    await tester.pumpWidget(AppWidget(controller: imcController));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

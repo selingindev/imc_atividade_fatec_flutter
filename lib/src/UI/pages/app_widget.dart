2a62055164f46807c36c71785165f110e5a446b1
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-14 14:06:07 -0300

    retirando debug icon

--- lib/src/UI/pages/app_widget.dart
+++ lib/src/UI/pages/app_widget.dart
@@ -14,6 +14,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),

5d5ce21b12901b2379912b3c98218b515957bdab
Arthur Selingin <arthur_santos@outlook.com.br>
2024-09-14 14:03:08 -0300

    logica caso receba 0 no input

--- lib/src/aplication/controllers/imc_controller.dart
+++ lib/src/aplication/controllers/imc_controller.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:imc_atividade_fatec_flutter/core/calculate_imc_usecase.dart';
@@ -10,21 +9,21 @@ class ImcController {
 
   ImcController(this.calculateImcUsecase);
 
-  double? calculeIMC(){
-    
-    try{
+  double? calculeIMC() {
+    try {
       double weightDoubleController = double.parse(controllerALT.text);
       double heightDoubleController = double.parse(controllerPES.text);
-       if (weightDoubleController > 3.5) {
-      weightDoubleController = weightDoubleController / 100;  // Converte para metros
-    }
-     log(weightDoubleController);
-      return calculateImcUsecase.executeCount(weightDoubleController, heightDoubleController);
-
-    }catch (e){
-      
+      if (weightDoubleController > 3.5) {
+        weightDoubleController =
+            weightDoubleController / 100; // Converte para metros
+      }
+      if(weightDoubleController == 0.00 || heightDoubleController == 0.00){
+        return 0;
+      }
+      return calculateImcUsecase.executeCount(
+          weightDoubleController, heightDoubleController);
+    } catch (e) {
       return null;
     }
   }
-
 }
